4f6c766dcc023b3559286e7fa9b6f4dbe07e0e54
André Dupont <andredupontg@hotmail.com>
2022-06-22 16:41:49 -0500

    ADD: Se añade el schema y modelo de Publicacion

---
+++ models/Publicacion.js
@@ -0,0 +1,30 @@
+const {Schema, model} = require("mongoose");
+const {UsuarioSchema} = require("./Usuario");
+const {ServicioSchema} = require("./Servicio");
+
+const PublicacionSchema = new Schema({
+    usuario: {
+        type: UsuarioSchema,
+        required: true
+    },
+    servicio: {
+        type: ServicioSchema,
+        required: true
+    },
+
+    fecha_publicacion: {
+        type: Date,
+        required: true
+    },
+    costo: {
+        type: Number,
+        required: true
+    },
+});
+
+const Publicacion = model("Publicacion", PublicacionSchema);
+
+module.exports = {
+    PublicacionSchema,
+    Publicacion
+};
