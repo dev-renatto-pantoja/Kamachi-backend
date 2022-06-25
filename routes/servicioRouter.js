db319b7a9a474e094d68c52f4f24125ed6c14bf6
André Dupont <andredupontg@hotmail.com>
2022-06-24 21:38:31 -0500

    ADD: Se añaden las rutas de buscarServicio, actualizarInfo y eliminarServicio

--- routes/servicioRouter.js
+++ routes/servicioRouter.js
@@ -1,10 +1,40 @@
 const { Router } = require('express');
 const router = Router();
-const {listServices} = require("../controllers/servicioController");
+const {listServices, findService, removeService, updateInfo} = require("../controllers/servicioController");
+const {check} = require("express-validator");
+const {validarCampos} = require("../middlewares/validar-campos");
 
 router.get(
   '/listarServicios',
-  listServices
+    listServices
+);
+
+router.get(
+    '/buscarServicio',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    findService
+);
+
+router.patch(
+    '/actualizarInfo',
+    [
+        check('sector', 'El sector es obligatorio').not().isEmpty(),
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    updateInfo
+);
+
+router.delete(
+    '/eliminarServicio',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    removeService
 );
 
 module.exports = router;
