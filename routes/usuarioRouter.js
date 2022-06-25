4610f10ffe5807a09845ebb1023682158a225192
André Dupont <andredupontg@hotmail.com>
2022-06-24 21:37:26 -0500

    ADD: Se añaden las rutas listarUsuarios, buscarUsuario, eliminarUsuario y se corrige actualizarInfo

--- routes/usuarioRouter.js
+++ routes/usuarioRouter.js
@@ -1,19 +1,42 @@
 const { Router } = require('express');
 const router = Router();
-const {updateInfo} = require("../controllers/usuarioController");
+const {listUsers, findUser, removeUser, updateInfo} = require("../controllers/usuarioController");
 const {check} = require("express-validator");
 const {validarCampos} = require("../middlewares/validar-campos");
+
+router.get(
+    '/listarUsuarios',
+    listUsers
+);
+
+router.get(
+    '/buscarUsuario',
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        validarCampos
+    ],
+    findUser
+);
 
 router.patch(
     '/actualizarInfo',
     [
-        check('email', 'El email es obligatorio').isEmail(),
-        check('telefono', 'El nombre telefono es obligatorio').exists,
-        check('distrito', 'El distrito es obligatorio').exists,
-        check('rol', 'El rol es obligatorio').exists,
-        validarCampos
+            check('email', 'El email es obligatorio').isEmail(),
+            check('telefono', 'El nombre es obligatorio').exists,
+            check('distrito', 'El distrito es obligatorio').exists,
+            check('rol', 'El rol es obligatorio').exists,
+            validarCampos
     ],
     updateInfo
+);
+
+router.delete(
+    '/eliminarUsuario',
+    [
+            check('email', 'El email es obligatorio').isEmail(),
+            validarCampos
+    ],
+    removeUser
 );
 
 module.exports = router;
