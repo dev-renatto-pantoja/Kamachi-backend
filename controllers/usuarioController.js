b4eb6c38bd7f64f2983b7dc5b511f4792a676561
André Dupont <andredupontg@hotmail.com>
2022-06-24 21:34:14 -0500

    ADD: Se añaden las funciones listUsers, findUsers y removeUsers

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,56 @@
 const { response } = require('express');
-const Usuario = require("../models/Usuario");
+const {Usuario} = require("../models/Usuario");
+
+const listUsers = async (req, res = response) => {
+    try {
+        let users = [];
+        users = await Usuario.find();
+        if (null != users) {
+            return res.json({
+                ok: true,
+                usuarios: users
+            })
+        }
+    } catch (error) {
+        return res.status(400).json({
+            ok: false,
+            msg: "No se pudo listar todos los usuarios"
+        })
+    }
+}
+
+const findUser = async (req, res = response) => {
+    try {
+        const {email} = req.body;
+        let user = await Usuario.findOne({email});
+        if (null != user) {
+            return res.json({
+                ok: true,
+                usuario: user
+            })
+        }
+    } catch (error){
+        return res.status(400).json({
+            ok: false,
+            msg: "No se pudo encontrar el usuario"
+        })
+    }
+}
+
+const removeUser = async (req, res = response) => {
+    try {
+        const {email} = req.body;
+        let user = await Usuario.findOne({email});
+        if (null != email) {
+            user.remove();
+        }
+    } catch (error) {
+        return res.status(400).json({
+            ok: false,
+            msg: "No se pudo eliminar el usuario"
+        })
+    }
+}
 
 const updateInfo = async (req, res = response) => {
     try {
@@ -24,5 +75,8 @@ const updateInfo = async (req, res = response) => {
 }
 
 module.exports = {
+    listUsers,
+    findUser,
+    removeUser,
     updateInfo
 };
