8583abf22dded7656aee21cc3228e3a3e3bd129f
André Dupont <andredupontg@hotmail.com>
2022-06-30 20:49:35 -0500

    ADD: Se crea el schema y model de Historial de publicaciones

---
+++ models/Historial.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+const {UsuarioSchema} = require("./Usuario");
+const {PublicacionSchema} = require("./Publicacion");
+
+const HistorialSchema = new Schema({
+    usuario: {
+        type: UsuarioSchema,
+        require: true,
+    },
+    publicaciones: [{
+        type: PublicacionSchema,
+        require: true,
+    }]
+});
+
+const Historial = model("Historial", HistorialSchema);
+
+module.exports = { Historial, HistorialSchema };
